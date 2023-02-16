9ec11a9c99702c8b0c0046e8d8239d1ee2790c01
yumichaela <yumirohh@gmail.com>
2023-02-16 11:26:36 +0900

    0216

--- 집값예측.py
+++ 집값예측.py
@@ -1,20 +1,23 @@
-import pandas as pd
+import pandas as pd # pandas : 데이터를 데이터 프레임 형태로 다룰수 있음(보다 안정적이고 쉬움), 다양한 통계 함수 + 시각화 기능 갖춤
 import numpy as np
 from sklearn.model_selection import train_test_split
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader, Dataset
 import torch.nn.functional as F
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error # MSE(MeanSquaredError) : 미분 계산이 쉬워서 모델 최적화에 자주 사용하는 손실 함수임. But 제곱근 씌워 나와서 정확하지X > 루트 씌운게 > RMSE
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('./data/reg.csv', index_col=[0])
+# 데이터 세트 만들기
+df = pd.read_csv('./data/reg.csv', index_col=[0]) #reas_csv : 스케일링된 집값 데이터 불러옴 / index_col=[0] : csv파일의 1번째 열의 데이터 인덱스를 제외하고, 데이터 프레임 만듬
 
-X = df.drop('Price', axis=1).to_numpy()
-Y = df['price'].to_numpy().reshape((-1,1))
+#데이터 프레임을 Numpy 배열로 만들기
+X = df.drop('Price', axis=1).to_numpy() # (데이터 프레임) df에서 Price를 제외한 나머지를 변수로 사용함 / axis=1 : 열을 의미함 > Price를 열 기준을 제외하겠다는 뜻임
+Y = df['price'].to_numpy().reshape((-1,1)) # Price를 타겟값 Y로 사용 하겠다는 뜻임
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5) # 전체 데이터를 50:50으로 > 학습 데이터 + 평가 데이터로 나눔
 
+# 텐서 데이터 만들기
 class TensorData(Dataset):
     def __init__(self, x_data, y_data):
         self.x_data = torch.FloatTensor(x_data)
@@ -35,3 +38,4 @@ testloader = torch.utils.data.DataLoader(trainsets, batch_size=32, suffle=False)
 class Regressor(nn.Module):
     def __init__(self):
         super().__init__()
+        self.
